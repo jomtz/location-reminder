74a7f172c782f98180c857bc29347a2674ec164c
Josue Martinez <46319076+josuemrnez@users.noreply.github.com>
2021-10-12 12:06:14 -0400

    Add Register screen

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,15 +1,13 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import androidx.lifecycle.Observer
 import com.firebase.ui.auth.AuthUI
-import com.udacity.project4.R
+import com.firebase.ui.auth.IdpResponse
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
 import com.udacity.project4.locationreminders.RemindersActivity
 
@@ -46,6 +44,18 @@ class AuthenticationActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                remindersActivityIntent()
+            } else {
+                return
+            }
+        }
     }
 
     private fun launchSignInFlow() {
